2426294caf0ecd6c4268ed56ac5bb245b8bae938
renato_albuquerque <renatomalb@hotmail.com>
2025-10-29 23:43:08 -0300

    transformacoes tabela staging.st_movimentos, colunas tipo e valor

---
+++ db_cash_flow.sql
@@ -0,0 +1,47 @@
+-- Visualizar tabelas db_cash_flow, schema public (Raw data).
+select * from public.bancos;
+select * from public.contas;
+select * from public.movimentos limit 50;
+select * from public.saldo;
+
+-- Criar schema staging.
+create schema staging;
+
+-- Copiar tabelas do schema public (Raw data) para o schema staging.
+create table staging.st_bancos as table public.bancos;
+create table staging.st_contas as table public.contas;
+create table staging.st_movimentos as table public.movimentos;
+create table staging.st_saldo as table public.saldo;
+
+-- Visualizar tabelas db_cash_flow, schema public (Raw data).
+select * from staging.st_bancos;
+select * from staging.st_contas;
+select * from staging.st_movimentos limit 50;
+select * from staging.st_saldo;
+
+-- Transformações
+
+-- Transformação tabela st_movimentos (Staging).
+
+
+-- Transformação tabela st_movimentos (Staging).
+-- Coluna Tipo: Entradas = E, Saídas = S.
+UPDATE staging.st_movimentos
+SET "Tipo" = 'E'
+WHERE "Tipo" = 'Entradas';
+
+UPDATE staging.st_movimentos
+SET "Tipo" = 'S'
+WHERE "Tipo" = 'Saídas';
+
+select * from staging.st_movimentos;
+
+-- Coluna Valor, transformar para "Número decimal fixo".
+ALTER TABLE staging.st_movimentos
+ALTER COLUMN "Valor" TYPE numeric(15,2)
+USING ROUND("Valor"::numeric, 2);
+
+select * from staging.st_movimentos;
+
+
+
